a750529fbcbc9c07c51ee9aa974c600365c106b5
dnkrit <dnkrit@yandex.ru>
2025-05-05 11:10:56 +0300

    new_django

--- django_lesson_project_fresh/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('new', views.new, name='page2'),
-]
--- zerocoder/zerocoder/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # <--- подключение всех путей из main/urls.py
-]
